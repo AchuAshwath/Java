9a3328eee14f06828a8f07edbca645301197b6fc
AchuAshwath <achuashwath88@gmail.com>
2024-09-11 10:14:22 +0530

    brojava vid ignore and printf

--- BroJava/6_methods.java
+++ BroJava/6_methods.java
@@ -4,6 +4,10 @@ class JavaMethods {
     public static void main(String[] args) {
         // method is a set of instruction/ block of code - {}, that is executed when it it called.
         helloWorld();
+        StringhelloWorld("Ashwath");
+        System.out.println("calculating sum of 1 and 3 using the sum function : "+sum(1,3));
+        System.out.println("Overloaded function of sum in double data type : "+sum(0.69, 0.69));
+        System.out.println("sum of three numbers 1,2,3 : "+sum(1, 2, 3 ));
 
         // cannot make a static reference to a non static method from the type JavaMethods
 
@@ -12,4 +16,23 @@ class JavaMethods {
     static void helloWorld(){
         System.out.println("Hello, World!");
     }
+
+    static void StringhelloWorld(String Name){
+        System.out.println("Hello, "+ Name+ "!");
+    }
+
+    static int sum(int number1, int number2){
+        return number1+ number2;
+    }
+    // overloaded functions = methods that share the same name but have different parameters
+    //                        method name + parameters = method signature
+
+    static double sum(double number1, double number2){
+        return number1 + number2;
+    }
+
+    static int sum(int number1, int number2, int number3){
+        return number1+number2+number3;
+    }
 }
+
---
+++ BroJava/7_printf.java
@@ -0,0 +1,63 @@
+package BroJava;
+
+class printFormat{
+    public static void main(String[] args){
+        // printf() = an optional method to control, format, and display text to the console window
+        //            two arguments = format string + (object/variable/value)
+        //            % [flags] [precision] [width] [conversion-character]
+        //            % = indicates a variable
+        //            flags = adds an effect to output (+, -, 0, ', etc.)
+        //            precision = number of decimal places
+        //            width = number of characters to display
+        //            [conversion-character] = d = decimal integer [byte, short, int, long]
+        //                                    f = floating point number [float, double]
+        //                                    c = character
+        //                                    s = string
+        //                                    h = hashcode
+        //                                    t = date/time
+        //                                    % = output a % sign
+
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Ashwath";
+        int myInt = 50;
+        double myDouble = 1000;
+
+        // [conversion-character]
+        System.out.printf("%b\n", myBoolean); // %b = boolean
+        System.out.printf("%c\n", myChar); // %c = char
+        System.out.printf("%s\n", myString); // %s = string
+        System.out.printf("%d\n", myInt); // %d = int
+        System.out.printf("%f\n", myDouble); // %f = double
+
+        // [width]
+        System.out.printf("Hello %10s\n", myString); // %10s = 10 characters wide
+        System.out.printf("Hello %-10s\n", myString); // %-10s = 10 characters wide, left-justified
+
+        // [precision]
+        System.out.printf("You have this much money %.2f\n", myDouble); // %.2f = 2 decimal places
+
+        // [flags]
+        System.out.printf("You have this much money %,f\n", myDouble); // %,f = use , to group numbers
+        System.out.printf("You have this much money %,.2f\n", myDouble); // %,.2f = use , to group numbers, 2 decimal places
+
+        // [argument-index$]
+        System.out.printf("%2$s %2$s %1$s %2$s %1 $s\n", "beers", "99"); // 1$ = first argument, 2$ = second argument
+
+        // [flags] = adds an effect to output
+        // - = left-justify
+        // + = output a plus ( + ) or minus ( - ) sign for a numeric value
+        // 0 = numeric values are zero-padded
+        // , = comma grouping separator if numbers > 1000
+        // ( = encloses negative numbers in parentheses
+        // # = add 0, 0x, 0X, 0b, 0B prefixes
+        // space = leave a space for positive numbers
+        System.out.printf("%,d\n", 1000000000);
+        System.out.printf("%(d\n", -1000000000);
+        System.out.printf("%+,d\n", 1000000000);
+        System.out.printf("%+,d\n", -1000000000);
+        System.out.printf("%010d\n", 1000);
+        System.out.printf("%,010d\n", 1000);
+        System.out.printf("%,010d\n", 1000000000);
+    }
+}
