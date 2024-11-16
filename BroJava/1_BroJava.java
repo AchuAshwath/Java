49d554d27f1755d300077d60c6f0c442ac437889
= <achuashwath88@gmail.com>
2024-11-16 20:25:11 +0530

    added some quality life and gitignore

--- BroJava/1_BroJava.java
+++ BroJava/1_BroJava.java
@@ -1,101 +1,100 @@
 package BroJava;
+
 import java.util.Random;
 
 class BroJava {
-    public static void main(String[] args) {
-        //  java source code - .java file 
-        //  complier
-        //  byte code - .class file - 
-        //  JVM
-        //  machine code - 0s and 1s
+  public static void main(String[] args) {
+    // java source code - .java file
+    // complier
+    // byte code - .class file -
+    // JVM
+    // machine code - 0s and 1s
 
-        //  JDK - Java Development Kit
-        //  JRE - Java Runtime Environment
-        //  JVM - Java Virtual Machine
-        
-        System.out.     println("Hello, World!");
+    // JDK - Java Development Kit
+    // JRE - Java Runtime Environment
+    // JVM - Java Virtual Machine
 
-        // data type     size      primitive/reference                   value
-        // boolean       1 bit      primitive               false
-        // byte         1 byte      primitive               -128 to 127
-        // short        2 bytes     primitive               -32,768 to 32,767
-        // int          4 bytes     primitive               -2 billion to 2 billion    
-        // long         8 bytes     primitive               -9 quintillion to 9 quintillion
-        // float        4 bytes     primitive               fractional number up to 6-7 digits
-        // double       8 bytes     primitive               fractional number up to 15 digits
-        // char         2 bytes     primitive               single character/ letter/ASCII value
-        // String       varies      reference               a sequence of characters
+    System.out.println("Hello, World!");
 
-        // primitive                                            reference
-        // 8 types (boolean, byte, etc.)                       unlimited (user defined)
-        // stores data                                         stores an address
-        // can only hold 1 value                               could hold more than 1 value
-        // less memory                                         more memory
-        // fast                                                slower
+    // data type size primitive/reference value
+    // boolean 1 bit primitive false
+    // byte 1 byte primitive -128 to 127 (1 byte is 8 bit thus 2^8 = 256 values
+    // ranging from)
+    // short 2 bytes primitive -32,768 to 32,767
+    // int 4 bytes primitive -2 billion to 2 billion
+    // long 8 bytes primitive -9 quintillion to 9 quintillion
+    // float 4 bytes primitive fractional number up to 6-7 digits
+    // double 8 bytes primitive fractional number up to 15 digits
+    // char 2 bytes primitive single character/ letter/ASCII value
+    // String varies reference a sequence of characters
 
+    // primitive reference
+    // 8 types (boolean, byte, etc.) unlimited (user defined)
+    // stores data stores an address
+    // can only hold 1 value could hold more than 1 value
+    // less memory more memory
+    // fast slower
 
-        // Variables
+    // Variables
 
-        int variableName; // declaration    
+    int variableName; // declaration
 
-        variableName = 10; // assignment
+    variableName = 10; // assignment
 
-        String Name = "Ashwath N"; // initialization
+    String Name = "Ashwath N"; // initialization
 
-        System.out.println("Hi!, My name is, "+Name);
-        System.out.println("and I assigned the value, "+variableName+" to the variable, variableName");
+    System.out.println("Hi!, My name is, " + Name);
+    System.out.println("and I assigned the value, " + variableName + " to the variable, variableName");
 
-        // variable swapping
+    // variable swapping
 
-        int a = 10;
-        int b = 20;
-        
-        System.out.println("Before swapping, a = "+a+" and b = "+b);
+    int a = 10;
+    int b = 20;
 
-        int temp = a;
-        a = b;
-        b = temp;
+    System.out.println("Before swapping, a = " + a + " and b = " + b);
 
-        System.out.println("After swapping, a = "+a+" and b = "+b);
+    int temp = a;
+    a = b;
+    b = temp;
 
-        // random values
-        Random random = new Random();   // thats kind of random
+    System.out.println("After swapping, a = " + a + " and b = " + b);
 
-        //these are not true random numbers, they are pseudo random numbers
+    // random values
+    Random random = new Random(); // thats kind of random
 
-        int randomNumber = random.nextInt(69,96); // generates a random number between 69 and 96 
-        double randomDouble = random.nextDouble(); // generates a random double value between 0.0 and 1.0
-        boolean randomBoolean = random.nextBoolean(); // generates a random boolean value
-        System.out.println("Random number between 69 and 96 is : "+randomNumber);
-        System.out.println("Random double value is : "+randomDouble);
-        System.out.println("Random boolean value is : "+randomBoolean);
+    // these are not true random numbers, they are pseudo random numbers
 
-        // constants
-        final int constantName = 10; // final keyword is used to declare a constant
+    int randomNumber = random.nextInt(69, 96); // generates a random number between 69 and 96
+    double randomDouble = random.nextDouble(); // generates a random double value between 0.0 and 1.0
+    boolean randomBoolean = random.nextBoolean(); // generates a random boolean value
+    System.out.println("Random number between 69 and 96 is : " + randomNumber);
+    System.out.println("Random double value is : " + randomDouble);
+    System.out.println("Random boolean value is : " + randomBoolean);
 
-        // constantName = 20; // this will throw an error
+    // constants
+    final int constantName = 10; // final keyword is used to declare a constant
 
-        // 2d array
-        int matrix[][] = new int[3][3]; // declaration of a 3x3 matrix
+    // constantName = 20; // this will throw an error
 
-        matrix[0][0] = 1;
-        matrix[0][1] = 2;
-        matrix[0][2] = 3;
-        matrix[1][0] = 4;
-        matrix[1][1] = 5;
-        matrix[1][2] = 6;
-        matrix[2][0] = 7;
-        matrix[2][1] = 8;
-        matrix[2][2] = 9;
+    // 2d array
+    int matrix[][] = new int[3][3]; // declaration of a 3x3 matrix
 
-        for (int[] elements: matrix){
-            for (int element: elements){
-                System.out.print(element+" ");
-            }
-            System.out.println();
-        }
+    matrix[0][0] = 1;
+    matrix[0][1] = 2;
+    matrix[0][2] = 3;
+    matrix[1][0] = 4;
+    matrix[1][1] = 5;
+    matrix[1][2] = 6;
+    matrix[2][0] = 7;
+    matrix[2][1] = 8;
+    matrix[2][2] = 9;
 
-        
-
+    for (int[] elements : matrix) {
+      for (int element : elements) {
+        System.out.print(element + " ");
+      }
+      System.out.println();
     }
-}        
+
+  }
+}
--- BroJava/4_wrapperClass.java
+++ BroJava/4_wrapperClass.java
@@ -1,48 +1,55 @@
 package BroJava;
-class wrapperClass{
-    public static void main(String[] args) {
-        // wrapper class - provides a way to use primitive data types as a reference data types
-                        // reference data types contains useful methods
-                        // can be used with collections (ArrayList)
 
-        // Primitive        Wrapper
-        // boolean          Boolean
-        // char             Character
-        // int              Integer
-        // double           Double
+class wrapperClass {
+  public static void main(String[] args) {
+    // wrapper class - provides a way to use primitive data types as a reference
+    // data types
+    // reference data types contains useful methods
+    // can be used with collections (ArrayList)
 
-        // autoBoxing -  the automatic conversion that the Java compiler makes between the primitive 
-        // types and their corresponding object wrapper classes
+    // Primitive Wrapper
+    // boolean Boolean
+    // char Character
+    // int Integer
+    // double Double
 
-        // unBoxing - the reverse of autoBoxing. Automatically converting an object of a wrapper class 
-        // to its corresponding primitive type
+    // autoBoxing - the automatic conversion that the Java compiler makes between
+    // the primitive
+    // types and their corresponding object wrapper classes
 
-        // autoBoxing
-        Boolean booleanValue = true;
-        Character charValue = 'a';
-        Integer intValue = 10;
-        Double doubleValue = 10.5;
-        String stringValue = "Ashwath";
+    // unBoxing - the reverse of autoBoxing. Automatically converting an object of a
+    // wrapper class
+    // to its corresponding primitive type
 
-        // here the primitive type is compared with the Boolean reference type via the autoBoxing process
-        // the autoBoxing process converts the primitive type to the Boolean reference type
+    // autoBoxing
+    Boolean booleanValue = true;
+    Character charValue = 'a';
+    Integer intValue = 10;
+    Double doubleValue = 10.5;
+    String stringValue = "Ashwath";
 
-        // since reference data types contains useful methods
-        // variable.{list of methods}
+    // here the primitive type is compared with the Boolean reference type via the
+    // autoBoxing process
+    // the autoBoxing process converts the primitive type to the Boolean reference
+    // type
 
-        System.out.println("int's floatvalue method : "+intValue.floatValue());
-         
-        // unBoxing
-        int unBoxedIntValue = intValue;
-        System.out.println("unBoxedIntValue : "+unBoxedIntValue);
+    // since reference data types contains useful methods
+    // variable.{list of methods}
 
-        if(booleanValue==true){
-            System.out.println("booleanValue is true");
-        }
+    System.out.println("int's floatvalue method : " + intValue.floatValue());
 
-        // here the Boolean reference type is compared with the primitive type via the unBoxing process
-        // the unBoxing process converts the Boolean reference type to the primitive type
+    // unBoxing
+    int unBoxedIntValue = intValue;
+    System.out.println("unBoxedIntValue : " + unBoxedIntValue);
 
-        
+    if (booleanValue == true) {
+      System.out.println("booleanValue is true");
     }
+
+    // here the Boolean reference type is compared with the primitive type via the
+    // unBoxing process
+    // the unBoxing process converts the Boolean reference type to the primitive
+    // type
+
+  }
 }
